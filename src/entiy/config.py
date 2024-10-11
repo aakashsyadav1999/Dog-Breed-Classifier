23cb2835d665bd449b181cbef5a64618ace87f1e
Aakash Yadav <aakashsyadav1999@gmail.com>
2024-10-12 00:14:47 +0530

    Added Data transformation Class

--- src/entiy/config.py
+++ src/entiy/config.py
@@ -15,9 +15,24 @@ class AWS_DOWNLOAD_CRED:
         self.parent_directory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
         # Define the new directory path outside of the research directory
         self.download_directory = os.path.join(self.parent_directory, AWS_DOWNLOAD_DATA_DIR)
-
-
-
+        # AWS Credentials
         self.AWS_KEY = os.getenv('AWS_KEY')
         self.SECRET_KEY = os.getenv('SECRET_KEY')
         self.BUCKET_NAME = BUCKET_NAME
+
+@dataclass
+class DATA_TRANSFORMATION : 
+
+    def __init__(self):
+        # Size of the image
+        self.SIZE = SIZE
+        # Batch Size
+        self.BATCH_SIZE = BATCH_SIZE
+        # Number of classes
+        self.NUM_CLASSES = NUM_CLASSES
+        # Learning Rate
+        self.LEARNING_RATE = LEARNING_RATE
+        # Dropout Rate
+        self.DROPOUT_RATE = DROPOUT_RATE
+        # Number of Epochs
+        self.EPOCHS = EPOCHS

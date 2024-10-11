0d217e9644ca3bc5d05114f10189c925a97a9204
Aakash Yadav <aakashsyadav1999@gmail.com>
2024-10-12 00:14:13 +0530

    Added Data transformation class

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -12,3 +12,12 @@ LOGS_FILE_NAME = "SIDFC.log"
 #AWS Download Data File
 AWS_DOWNLOAD_DATA_DIR = "data"
 BUCKET_NAME = "dog-breed-dataset"
+
+
+#Data Transformation
+SIZE = (350,350)
+BATCH_SIZE = 32
+NUM_CLASSES = ""
+LEARNING_RATE = 0.001
+DROPOUT_RATE = 0.7
+EPOCHS = 5
